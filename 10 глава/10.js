13b3dc31a9dd0292e2564bcba5bb2921a48163c5
Pavel <paha020798@mail.ru>
2025-03-22 11:14:41 +0700

    try...catch pre

--- 10 глава/10.js
+++ 10 глава/10.js
@@ -22,11 +22,188 @@ console.log(safeDivide(5, 0)); // Ошибка
 
 function parseJson(str) {
   try {
-    return JSON.parse(str);
+    const parse = JSON.parse(str);
+    return parse;
   } catch (err) {
-    console.log("формат некорректный");
+    console.log("формат некорректный", err.message);
+    return null;
   }
 }
+console.log(parseJson('{"name" : "Pavel"}'));
+
+// 3.
+// Напишите функцию `getElement(array, index)`, которая принимает массив и индекс. Она должна возвращать элемент массива по указанному индексу.
+// Если индекс выходит за границы массива, выбрасывайте ошибку с сообщением "Индекс вне границ массива". Используйте `try..catch` для обработки этой ошибки и возвращайте `undefined` в случае её возникновения.
+
+function getElement(arr, i) {
+  try {
+    if (i < 0 || i >= arr.length) {
+      throw new Error("Индекс вне границ массива");
+    }
+  } catch (err) {
+    return err.message;
+  }
+
+  return arr[i];
+}
+const arr = [1, 2, 3, 4, 5];
+console.log(getElement(arr, 1)); // 2
+console.log(getElement(arr, 15)); // Индекс вне границ массива
+
+//4.
+// Создайте функцию `sumNumbers(a, b)`, которая принимает два аргумента и возвращает их сумму. Если один из аргументов не является числом, выбрасывайте ошибку с сообщением "Оба аргумента должны быть числами".
+// Обработайте ошибку с помощью `try..catch` и возвращайте `0` в случае возникновения ошибки.
+function sumNumbers(a, b) {
+  try {
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("Оба аргумента должны быть числами");
+    }
+  } catch (e) {
+    console.log(e.message);
+    return 0;
+  }
+  return a + b;
+}
+
+console.log(sumNumbers(1, 2)); //3
+console.log(sumNumbers("qwer", 5)); // Оба аргумента должны быть числами
+//5.
+//  Напишите функцию `getProperty(obj, prop)`, которая принимает объект и имя свойства. Она должна возвращать значение свойства объекта. Если переданное свойство не существует, выбрасывайте ошибку с сообщением "Свойство не найдено".
+// Обработайте эту ошибку с использованием `try..catch` и возвращайте `null` в случае возникновения ошибки.
+
+const user = {
+  name: "pavel",
+  age: 555,
+  location: "zopa",
+};
+function getProperty(obj, prop) {
+  try {
+    if (!obj.hasOwnProperty(prop)) {
+      throw new Error("Свойство не найдено");
+    }
+  } catch (e) {
+    console.log(e.message);
+    return null;
+  }
+  return obj[prop];
+}
+
+console.log(getProperty(user, "location")); // zopa
+console.log(getProperty(user, "city")); // Свойство не найдено null
+//6.
+// Напишите функцию `validateEmail(email)`, которая принимает строку с электронной почтой и проверяет, соответствует ли она стандартному формату (например, с использованием регулярного выражения). Если формат неверен, выбрасывайте ошибку с сообщением "Некорректный адрес электронной почты".
+// Обработайте ошибку с помощью 'try..catch' и возвращайте `false` в случае её возникновения. В противном случае возвращайте `true`.
+
+function validateEmail(email) {
+  try {
+    const regExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regExp.test(email)) {
+      throw new Error("Некорректный адрес электронной почты");
+    }
+  } catch (e) {
+    console.log(e.message);
+
+    return false;
+  }
+  return true;
+}
+
+console.log(validateEmail("test@example.com")); // true
+console.log(validateEmail("invalid-email"));
+"Некорректный адрес электронной почты", false;
+// 7.
+// Напишите функцию validateNumbers, которая принимает массив чисел. В случае, если в массиве есть хотя бы одно нечисловое значение,
+// выбросьте TypeError с сообщением "Все элементы массива должны быть числами!". Оберните вызов этой функции в try...catch, чтобы обработать возможные исключения.
+
+function validateNumbers(arr) {
+  if (arr.some((el) => typeof el !== "number")) {
+    throw new TypeError("Все элементы массива должны быть числами!");
+  }
+  console.log("Все элементы массива являются числами.");
+}
+try {
+  console.log(validateNumbers([1, 2, "qwe"]));
+} catch (e) {
+  console.log(e.message);
+}
+
+// 8.
+// Создайте класс InvalidAgeError, который наследует от Error. Реализуйте функцию registerUser, которая принимает возраст. Если возраст меньше 18,
+// выбросьте InvalidAgeError с сообщением "Возраст должен быть 18 или больше!". Используйте try...catch, чтобы обработать эту ошибку.
+
+class InvalidAgeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "InvalidAgeError";
+  }
+}
+function registerUser(age) {
+  try {
+    if (age < 18) {
+      throw new InvalidAgeError("Возраст должен быть 18 или больше");
+    }
+  } catch (e) {
+    // console.log(e);
+    console.log(e.message);
+    // console.log(e.name);
+  }
+}
+console.log(registerUser(11));
+
+// 9.
+// Создайте класс `UserValidationError`, расширяющий стандартный класс `Error`. Эта ошибка должна использоваться для проверки корректности данных пользователя.
+//  1. Определите класс `UserValidationError`, который принимает сообщение о чем-то, что не так с данными пользователя.
+// 2. Реализуйте функцию `validateUser`, которая принимает объект пользователя с полями `name` и `age`. Проверьте, что:
+//    - Имя пользователя является строкой и не пустое.
+//    - Возраст — это число, и оно больше нуля.
+// 3. Если условия не выполнены, выбросьте `UserValidationError` с соответствующим сообщением (например, "Имя должно быть непустой строкой" или "Возраст должен быть положительным числом").
+// 4. Напишите несколько тестов, в которых корректные и некорректные данные проверяются с использованием блоков `try...catch`.
+
+class UserValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UserValidationError";
+  }
+}
+
+function validateUser(obj) {
+  if (!obj.hasOwnProperty("name")) {
+    throw new UserValidationError("Отсутствует свойство 'name'");
+  }
+  if (obj.name === "") {
+    throw new UserValidationError("Имя должно быть непустой строкой");
+  }
+  if (typeof obj.name !== "string") {
+    throw new UserValidationError("Имя должно быть строкой");
+  }
+  if (!obj.hasOwnProperty("age")) {
+    throw new UserValidationError("Отсутствует свойство 'age'");
+  }
+  if (obj.age < 0) {
+    throw new UserValidationError("Возраст должен быть положительным числом");
+  }
+  if (typeof obj.age !== "number") {
+    throw new UserValidationError("Возраст должен быть числом");
+  }
+  return obj;
+}
+
+const users = [
+  { name: "Zopa", age: 25 }, // Пользователь "Zopa" прошел валидацию.
+  { name: "John", age: -5 },
+  { name: "Emma", age: 30 },
+  { age: 25 },
+  { name: "Liam" },
+];
+
+users.forEach((user) => {
+  try {
+    validateUser(user);
+    console.log(`Пользователь "${user.name}" прошел валидацию.`);
+  } catch (err) {
+    console.log(`${err.name}: ${err.message}`);
+  }
+});
 
 async function test() {
   try {
